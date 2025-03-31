a08a54fdab4f3b15246ba3b479724bafa28afec1
Adelin <email@email.com>
2025-03-31 13:54:26 +0300

    Integrate Board component into app layout, add board background color variable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ViewHeaderComponent } from './view-header/view-header.component';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ViewHeaderComponent],
+  imports: [HeaderComponent, ViewHeaderComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'task-management-app';
+
+  
 }
